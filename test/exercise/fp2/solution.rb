3cd30f74dff56d3ca8a3cfc4c82e05a82d0cb760
RinatKhabibullin <lmiksl@gmail.com>
2021-04-08 23:33:58 +0300

    add fp2 solution

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -1,20 +1,37 @@
 module Exercise
   module Fp2
     class MyArray < Array
-      # Использовать стандартные функции массива для решения задач нельзя.
-      # Использовать свои написанные функции для реализации следующих - можно.
+      def my_each(&func)
+        first_element, *rest_elements = self
+        rest_elements = MyArray.new(rest_elements)
+        func.call(first_element)
+        if rest_elements.empty?
+          first_element
+        else
+          [first_element, *rest_elements.my_each(&func)]
+        end
+      end
 
-      # Написать свою функцию my_each
-      def my_each; end
+      def my_map(&func)
+        handler = ->(acc, el) { MyArray.new([*acc, func.call(el)]) }
+        my_reduce([], &handler)
+      end
 
-      # Написать свою функцию my_map
-      def my_map; end
+      def my_compact
+        handler = ->(acc, el) { el.nil? ? acc : MyArray.new([*acc, el]) }
+        my_reduce([], &handler)
+      end
 
-      # Написать свою функцию my_compact
-      def my_compact; end
-
-      # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(acc = nil, &func)
+        first_element, *rest_elements = self
+        rest_elements = MyArray.new(rest_elements)
+        if rest_elements.empty?
+          func.call(acc, first_element)
+        else
+          acc = acc.nil? ? first_element : func.call(acc, first_element)
+          rest_elements.my_reduce(acc, &func)
+        end
+      end
     end
   end
 end
